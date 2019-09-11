ee7b7d11b12ef597a44d379d1f75018ae25df316
Yotam Tal <me@yotamtal.co>
2019-09-12 02:46:59 +0300

    Add slot to file message

--- src/messages/FileMessage.vue
+++ src/messages/FileMessage.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class='sc-message--file' :style="messageColors">
-    <div class='sc-message--file-icon'>
-      <img :src="data.file.url" class="sc-image">
+    <div class='sc-message--file' :style="messageColors">
+      <slot :message="data" :messageText="data.text" :messageColors="messageColors" :me="me">
+        <div class='sc-message--file-icon'>
+          <img :src="data.file.url" class="sc-image">
+        </div>
+        <div class='sc-message--file-name' :style="messageColors">
+          <a :href="data.file.url ? data.file.url : '#'" target='_blank'>{{data.file.name || ''}}</a>
+        </div>
+        <div class="sc-message--file-text" :style="messageColors">{{data.text}}<p v-if="data.meta" class='sc-message--meta' :style="messageColors">{{data.meta}}</p></div>
+      </slot>
     </div>
-    <div class='sc-message--file-name' :style="messageColors">
-      <a :href="data.file.url ? data.file.url : '#'" target='_blank'>{{data.file.name || ''}}</a>
-    </div>
-    <div class="sc-message--file-text" :style="messageColors">{{data.text}}<p v-if="data.meta" class='sc-message--meta' :style="messageColors">{{data.meta}}</p></div>
-  </div>
 </template>
 
 <script>
